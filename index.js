90dd947f7c6ff7986470452192839e4d22ba8281
Charles Francoise <charles.francoise@gmail.com>
2014-06-19 18:14:56 +0200

    first import

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var hideAnswer = function() {
+    $('#answer-box').removeClass('in');
+    $('#answer-box').addClass('out');
+};
+
+$(document).ready(function() {
+    $('#submit').click(function(){
+        $('#query').blur();
+
+        var query = $('#query')[0].value;
+
+        // Is it Lua?
+        if (query.toLowerCase() === 'lua') {
+            // It's Lua! Pierre likes this.
+            $('#answer-box').removeClass('out');
+            $('#answer-box').addClass('in');
+            $('#answer').text('Yes!');
+        } else {
+            // It isn't Lua. Pierre is sad.
+            $('#answer-box').removeClass('out');
+            $('#answer-box').addClass('in');
+            $('#answer').text('No.');
+        }
+    });
+
+    $('#query').focus(hideAnswer);
+    $('#query').change(hideAnswer);
+  });
